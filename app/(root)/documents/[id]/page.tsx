72deebd9d3a798a21c1aa3e824e56b10bfdc4593
sadoyin <sadoyin74@gmail.com>
2024-08-17 05:11:40 -0700

    second comit message

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,41 @@
+
+import Header from '@/components/Header'
+import { Editor } from '../../../../components/editor/Editor'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import CollaborativeRoom from '@/components/CollaborativeRoom'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import { getDocument } from '@/lib/actions/room.actions'
+import { getClerkUsers } from '@/lib/actions/user.actions'
+
+const Document = async ({params:{id}}: SearchParamProps) => {
+  const clerkUser = await currentUser()
+  if(!clerkUser) redirect('/sign-in')
+
+    const room = await getDocument({
+      roomId:id,
+      userId:clerkUser.emailAddresses[0].emailAddress,
+    })
+
+    if(!room) redirect('/')
+
+    const userIds=Object.keys(room.usersAccesses)
+    const users= await getClerkUsers({userIds})
+    const usersDatta = users.map((user:User)=>({
+      ...user,
+      userType:room.usersAccesses[user.email]?.includes('room:write') 
+        ? 'editor'
+        : 'viewer'
+    }))
+
+    const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes('room:write') ? 'editor' : 'viewer'
+
+
+  return (
+    <main className='flex w-full flex-col items-center'>
+      <CollaborativeRoom  roomId={id} roomMetadata={room.metadata} users={usersDatta} currentUserType={currentUserType}/>
+    </main>
+  )
+}
+
+export default Document
